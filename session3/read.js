de7e3500ab28cca370bf0ece865696e2ee3a78c2
Trinh Duc Tai <trinhductai6869@gmail.com>
2019-10-03 16:26:54 +0700

    delete all

--- session3/read.js
+++
@@ -1,9 +0,0 @@
-let menu = ["Com", "Canh", "Tom", "Rau", "Ca"];
-for(let i=0; i < menu.length; i++){
-    console.log(`${i+1}.${menu[i]}`)
-}
-
-//optional
-menu.forEach((food, index) => {
-    console.log(`${index+1}.${food}`)
-})
--- test.py
+++
@@ -1,8 +0,0 @@
-class Counter:
-    def _init_(self,count=0):
-        self.count = count
-    def tick(self):
-        return self.count + 1
-    def reset(self):
-        self.count = 0
-        return self.count
